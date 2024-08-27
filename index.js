ae68fb86cbef95bd825d0ef95625094b5495913a
Nofel-Esed <esedovnofel0038@gmail.com>
2024-08-27 15:05:18 +0300

    upload task

---
+++ index.js
@@ -0,0 +1,21 @@
+for(i=2; i<500; i++){
+    
+        for(let j=2;Math.floor(i/2);j++)
+    
+            if(i%j===0){
+                break;
+            }
+            else{
+                console.log(i);
+                break;
+            }
+    }
+    
+    
+
+const symbol = "+"
+for (let i =0; i <= 5; i++){
+    console.log(symbol.repeat(i))
+}
+
+// for(deyisken, sert, hereket)
